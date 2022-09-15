247918e8d210509ea805224d76303542f7b5c9eb
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2022-09-15 21:23:46 +0430

    fix: minimum macos version for data compression

--- Sources/DataExtensionKit/Compression.swift
+++ Sources/DataExtensionKit/Compression.swift
@@ -3,12 +3,12 @@ import Foundation
 public extension Data {
     /// These methods return a compressed or decompressed version of the receiver using the specified algorithm
     
-    @available(iOS 13.0, *)
+    @available(iOS 13.0, macOS 10.15, *)
     func compressed(using algorithm: NSData.CompressionAlgorithm) throws -> Data {
         try (self as NSData).compressed(using: algorithm) as Data
     }
 
-    @available(iOS 13.0, *)
+    @available(iOS 13.0, macOS 10.15, *)
     func decompressed(using algorithm: NSData.CompressionAlgorithm) throws -> Data {
         try (self as NSData).decompressed(using: algorithm) as Data
     }
