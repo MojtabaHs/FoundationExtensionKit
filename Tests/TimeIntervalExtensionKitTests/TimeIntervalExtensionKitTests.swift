32046459c28374a39377f89bb7259fe6e2b60ef3
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2022-09-15 21:02:27 +0430

    fix: naming type in test files

--- Tests/TimeIntervalExtensionKitTests/TimeIntervalExtensionKitTests.swift
+++ Tests/TimeIntervalExtensionKitTests/TimeIntervalExtensionKitTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import TimeIntervalExtensionKit
 
-final class StringExtensionKitTests: XCTestCase {
+final class TimeIntervalExtensionKitTests: XCTestCase {
     var testCasesForCreatingTimeIntervalsUsingDateComponents = [
         (input: DateComponents(second: 5)           , expectation: TimeInterval(5)),
         (input: DateComponents(second: 100)         , expectation: TimeInterval(100)),
